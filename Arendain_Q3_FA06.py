411ef5d90022288fd848dd4d1a5c490c83cc1d46
Raine <rainealthea.arendain@smc.pshs.edu.ph>
2026-01-26 10:15:39 +0800

    Create Arendain_Q3_FA06.py
    Student Profile Creator (Using Dictionary)

---
+++ Arendain_Q3_FA06.py
@@ -0,0 +1,15 @@
+students = {
+    "student": input("Enter your name: "),
+    "age": input("Enter your age: "),
+    "subject": input("Enter your favorite subject: ")
+}
+
+print(" ")
+
+print("Student Record: ")
+
+print(" ")
+
+print("Name: ", students["student"])
+print("Age: ", students["age"])
+print("Favorite Subject: ", students["subject"])
