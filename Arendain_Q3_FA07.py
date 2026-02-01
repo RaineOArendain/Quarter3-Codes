b8c9b2ac107624d92a201437e83bc4bd71445017
Raine <rainealthea.arendain@smc.pshs.edu.ph>
2026-02-01 21:50:37 +0800

    Add files via upload

---
+++ Arendain_Q3_FA07.py
@@ -0,0 +1,19 @@
+Steps = [
+    [4500, 5200, 4800, 5000, 5300],
+    [4000, 4100, 3900, 4200, 4600]
+]
+
+print("Total in each row:")
+print("1st: ", sum(Steps[0]), "|| 2nd: ", sum(Steps[1]))
+print(" ")
+print("Average in each row:")
+print("1st: ", sum(Steps[0])/len(Steps[0]), "|| 2nd: ", sum(Steps[1])/len(Steps[1]))
+
+
+# 1: Using an array helped me analyze the data more easily by helping me categorize each part. 
+# It also allowed me to simplify my code through functions and overall made my experience better.
+# Usin arrays helped me put everything together and help me easily find averages, totals, and more.
+
+# 2: Finding the total for each row was quite simple. Although it was confusing at first, I was able
+# to figure out what function I needed to use to get the total and how to use it on the corresponding row.
+# I was a little loss for finding the average but in the end I managed to figure it out.
